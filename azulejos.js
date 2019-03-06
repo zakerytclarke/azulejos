770e6c5950f2c54f7165c2da7607301d68b18ff0
Zakery Clarke <zakerytclarke@gmail.com>
2019-03-05 18:36:56 -0700

    Added round functionality

--- azulejos.js
+++ azulejos.js
@@ -21,13 +21,7 @@ function Game(numPlayers){
     this.turn=0;
     this.turnState="draw";
 
-    this.next=function(){
-      this.turnState="draw";
-      this.turn++;
-      if(this.turn>players.length){
-        turn=0;
-      }
-    }
+
 
     this.factoryboards=[
       [0,0,0,0],
@@ -77,17 +71,45 @@ function Game(numPlayers){
     this.discard=[];
 
 
-    var players=[];
+    this.players=[];
     for(var i=0;i<numPlayers;i++){
-      players.push(new Player());
+      this.players.push(new Player());
     }
 
     this.play=function(){
       this.setupFactories();
 
-      outputBoard(players[this.turn].board,this.factoryboards);
+      outputBoard(this.players[this.turn],this.factoryboards,this.turn);
 
     }
+    this.next=function(){
+      //Check if round is over
+      var bool=true;
+      for(var i=0;i<this.factoryboards.length-1;i++){
+        for(var j=0;j<this.factoryboards[i].length;j++){
+          bool&=this.factoryboards[i][j]==0;
+        }
+      }
+      if(bool){
+        for(var i=0;i<this.players.length;i++){
+          this.players[i].scoreBoard();
+        }
+
+        //Assign turn to whoever drew first
+      }else{
+        this.turn++;
+        if(this.turn>=this.players.length){
+          this.turn=0;
+        }
+      }
+
+
+      this.turnState="draw";
+
+
+      outputBoard(this.players[this.turn],this.factoryboards,this.turn);
+    }
+
     this.setupFactories=function(){
       for(var i=0;i<7;i++){
         for(var j=0;j<4;j++){
@@ -124,21 +146,37 @@ game.play();
 
 
 
+
 //Configure Tile Clicking
 for(var i=0;i<5;i++){
   for(var j=0;j<11;j++){
-    htmlboard[i][j].x=i;
+    htmlboard[i][j].y=i;
     htmlboard[i][j].x=j;
     htmlboard[i][j].onclick=function(){
       if(game.turnState=="place"){
-        game.turnState="draw";
-        game.next();
 
+        //TODO CHECK CORRECT COLOR
+
+        for(var k=0;k<this.y+1&&tileHolder.length!=0;k++){
+          if(game.players[game.turn].board[this.y][4-k]==0){//Open Space and Same Color
+            game.players[game.turn].board[this.y][4-k]=tileHolder.pop();
+          }
+        }
+        //Penalize for extra tiles
+        if(tileHolder.length!=0){
+
+        }
+        game.turnState="null";
+        outputBoard(game.players[game.turn],game.factoryboards,game.turn);
+
+        setTimeout(function(){game.next();},1000);
       }
     }
   }
 }
 
+
+//Assign Factory Click Function
 for(var i=0;i<game.factoryboards.length;i++){
   for(var j=0;j<game.factoryboards[i].length;j++){
     document.getElementById("factory"+i+":"+j).group=i;
@@ -150,10 +188,18 @@ for(var i=0;i<game.factoryboards.length;i++){
           this.style.border="3px black dashed";
           game.turnState="place";
           for(var k=0;k<game.factoryboards[this.group].length;k++){
-            if(color==game.factoryboards[this.group][k]){
+            //Remove from Factory
+            if(color==game.factoryboards[this.group][k]){//Move selected
               document.getElementById("factory"+this.group+":"+k).style.border="3px black dashed";
               game.factoryboards[this.group][k]=0;
               tileHolder.push(color);
+            }else{//Move Discards
+              var l=0;
+              while(game.factoryboards[7][l]!=0){
+                l++
+              }
+              game.factoryboards[7][l]=game.factoryboards[this.group][k];
+              game.factoryboards[this.group][k]=0;
             }
           }
         }
@@ -168,7 +214,11 @@ for(var i=0;i<game.factoryboards.length;i++){
 
 
 
-function outputBoard(board,factoryboards){
+function outputBoard(player,factoryboards,turn){
+  document.getElementById("score").innerHTML="Score: "+player.score;
+  document.getElementById("turn").innerHTML="Player "+(turn+1);
+
+  var board=player.board;
   for(var i=0;i<5;i++){
     for(var j=0;j<11;j++){
       htmlboard[i][j].style["background-color"]=colors[board[i][j]];
@@ -177,6 +227,7 @@ function outputBoard(board,factoryboards){
   for(var i=0;i<factoryboards.length;i++){
     for(var j=0;j<factoryboards[i].length;j++){
       document.getElementById("factory"+i+":"+j).style["background-color"]=colors[factoryboards[i][j]];
+      document.getElementById("factory"+i+":"+j).style.border="3px black solid";
     }
   }
 }
